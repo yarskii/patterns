6fa92e3f4ab9864ce1b8cdf6769d656489cf7395
yarskii <yarigusev@gmail.com>
2022-06-03 15:25:27 +0300

    LESSON_2

---
+++ framework/core.py
@@ -0,0 +1,57 @@
+class Application:
+
+    def __init__(self, urlpatterns: dict, front_controllers: list):
+
+        self.urlpatterns = urlpatterns
+        self.front_controllers = front_controllers
+
+    def parse_input_data(self, data: str):
+        result = {}
+        if data:
+            params = data.split('&')
+
+            for item in params:
+                k, v = item.split('=')
+                result[k] = v
+        return result
+
+    def parse_wsgi_input_data(self, data: bytes):
+        result = {}
+        if data:
+            data_str = data.decode(encoding='utf-8')
+            result = self.parse_input_data(data_str)
+        return result
+
+    def get_wsgi_input_data(self, env):
+        content_length_data = env.get('CONTENT_LENGTH')
+        content_length = int(content_length_data) if content_length_data else 0
+        data = env['wsgi.input'].read(content_length) if content_length > 0 else b''
+        return data
+
+    def __call__(self, env, start_response):
+        path = env['PATH_INFO']
+
+        if path[-1] != '/':
+            path = f'{path}/'
+
+        method = env['REQUEST_METHOD']
+        data = self.get_wsgi_input_data(env)
+        data = self.parse_wsgi_input_data(data)
+
+        query_string = env['QUERY_STRING']
+        request_params = self.parse_input_data(query_string)
+
+        if path in self.urlpatterns:
+            view = self.urlpatterns[path]
+            request = {}
+            request['method'] = method
+            request['data'] = data
+            request['request_params'] = request_params
+            for controller in self.front_controllers:
+                controller(request)
+            code, text = view(request)
+            start_response(code, [('Content-Type', 'text/html')])
+            return [text.encode('utf-8')]
+        else:
+            start_response('404 NOT FOUND', [('Content-Type', 'text/html')])
+            return [b'404 UNKNOWN COLOR!!!!!!1']
---
+++ main.py
@@ -0,0 +1,27 @@
+import views
+
+from framework.templates import render_
+from framework.core import Application
+
+routes = {
+    '/': views.index_view,
+    '/black/': views.black_view,
+    '/red/': views.red_view,
+    '/white/': views.white_view,
+    '/other/': views.Other(),
+    '/about/': views.about_view,
+    '/contact/': views.contact_view,
+    '/random/': views.random_view
+}
+
+
+def opposite_color_front(request):
+    request['opposite_color'] = 'opposite color'
+
+
+def similar_color_front(request):
+    request['similar_color'] = 'similar color'
+
+
+fronts = [opposite_color_front, similar_color_front]
+application = Application(routes, fronts)
