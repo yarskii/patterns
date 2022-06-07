218ab31e78dfadcc19c7f1dda4590e641f5fa129
yarskii <yarigusev@gmail.com>
2022-06-07 13:30:41 +0300

    LESSON_5_6_7

--- framework/templates.py
+++ framework/templates.py
@@ -1,9 +1,9 @@
-from jinja2 import Template, FileSystemLoader
+from jinja2 import FileSystemLoader
 from jinja2.environment import Environment
-import os
 
 
 def render_(template_name, folder='templates', **kwargs):
+
     env = Environment()
     env.loader = FileSystemLoader(folder)
     tmpl = env.get_template(template_name)
